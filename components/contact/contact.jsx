fdbb33e37ae06ce14d991c13d5998b88a6c4d6b9
Olivier <olivier_paspuel@hotmail.com>
2023-01-09 11:55:09 -0500

    contact me section

---
+++ components/contact/contact.jsx
@@ -0,0 +1,16 @@
+import StyledContact from "./styledContact";
+
+const Contact = () => {
+    return (
+        <StyledContact>
+            <h1>Get in touch</h1>
+            <ul className="information">
+                <li><a href="https://github.com/olivierpas13">Github</a></li>
+                <li><a href="">LinkedIn</a></li>
+                <li>olivier_paspuel@hotmail.com</li>rr
+            </ul>
+        </StyledContact>
+    );
+}
+ 
+export default Contact;
---
+++ components/contact/styledContact.js
@@ -0,0 +1,18 @@
+import styled from "styled-components"
+
+const StyledContact = styled.section`
+    background-color: white;
+    border-radius: 1em;
+    border: 1px solid #ccc;
+    box-shadow: 0px 0px 1px gray;
+    padding: 1em;
+    margin-top: 2em;
+    h2{
+        color: #111;
+        font-family: 'Courier New', Courier, monospace !important;
+        letter-spacing: 0.2em;
+    }
+
+`
+
+export default StyledContact;
